5a100b7aaed76b90522207eb6aa7376cce01247d
liamoar <avishr44@gmail.com>
2018-05-27 22:26:22 +0545

    login and siginup for mainpage,fragments also added

--- app/src/main/java/com/example/welcome/kati6/LoginActivity.java
+++ app/src/main/java/com/example/welcome/kati6/LoginActivity.java
@@ -1,23 +1,33 @@
 package com.example.welcome.kati6;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
-    Button about,signin,login;
-    EditText user,pass;
+    Button about, signin, login;
+    EditText user, pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        about=findViewById(R.id.btnabout);
-        signin=findViewById(R.id.btnsignin);
-        login=findViewById(R.id.btnlogin);
+        user = findViewById(R.id.etusername);
+        pass = findViewById(R.id.etpassword);
+        about = findViewById(R.id.btnabout);
+        signin = findViewById(R.id.btnsignin);
+        login = findViewById(R.id.btnlogin);
+
 
         about.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +40,85 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                View convertView = LayoutInflater.from(LoginActivity.this).inflate(R.layout.signup, null);
+                final EditText name = convertView.findViewById(R.id.etsignupusername);
+                final EditText pass = convertView.findViewById(R.id.etsignuppassword);
+                Button signin = convertView.findViewById(R.id.btnsignupsignin);
+
+
+                AlertDialog.Builder alert = new AlertDialog.Builder(LoginActivity.this);
+                alert.setView(convertView);
+                final AlertDialog dialog = alert.create();
+                dialog.show();
+
+                signin.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+                        String passaccess = pass.getText().toString();
+                        String useraccess = name.getText().toString();
+                        if (useraccess.isEmpty()) {
+                            name.setError("username is empty");
+                            name.requestFocus();
+                            return;
+                        }
+                        if (passaccess.isEmpty()) {
+                            pass.setError("password  is empty");
+                            pass.requestFocus();
+                            return;
+                        }
+
+                        SharedPreferences sp = getSharedPreferences("myfile", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sp.edit();
+
+                        String username = name.getText().toString();
+                        String password = pass.getText().toString();
+                        editor.putString("user1", username);
+                        editor.putString("pass1", password);
+                        editor.commit();
+
+                        Toast.makeText(LoginActivity.this, "Sign in successful", Toast.LENGTH_LONG).show();
+                        dialog.dismiss();
+
+                    }
+                });
+
             }
         });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String passaccess = pass.getText().toString();
+                String useraccess = user.getText().toString();
+                if (useraccess.isEmpty()) {
+                    user.setError("username is empty");
+                    user.requestFocus();
+                    return;
+                }
+
+                if (passaccess.isEmpty()) {
+                    pass.setError("password  is empty");
+                    pass.requestFocus();
+                    return;
+
+                }
+
+                String username = user.getText().toString();
+                String password = pass.getText().toString();
+                SharedPreferences sp = getSharedPreferences("myfile", Context.MODE_PRIVATE);
+                String myuser = sp.getString("user1", null);
+                String mypass = sp.getString("pass1", null);
+
+                if (myuser.equals(username) && mypass.equals(password)) {
+                    Toast.makeText(LoginActivity.this, "login successful", Toast.LENGTH_LONG).show();
+                    Intent i = new Intent(LoginActivity.this, MapActivity.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Sign in Failed", Toast.LENGTH_LONG).show();
+
+                }
 
             }
         });
---
+++ app/src/main/java/com/example/welcome/kati6/MapActivity.java
@@ -0,0 +1,81 @@
+package com.example.welcome.kati6;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MapActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+    EditText user;
+    private DrawerLayout drawer;
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maplayout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.map_layout);
+
+        NavigationView navigationView =findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new HomeFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+                break;
+            case R.id.nav_maps:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new MapFragment()).commit();
+                break;
+            case R.id.nav_fuelstation:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new FuelstationFragment()).commit();
+                break;
+            case R.id.nav_setting:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new SettingFragment()).commit();
+                break;
+            case R.id.nav_aboutus:
+                Toast.makeText(this,"about us toast",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_logout:
+
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+}
