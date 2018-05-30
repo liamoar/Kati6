ecd4723196e036deb1844b04ab76be2c1f90c85f
kessi17sabin <kessi17sabin@gmail.com>
2018-05-30 19:36:48 +0545

    setting ,mapstyle layout

--- app/src/main/java/com/example/welcome/kati6/SettingFragment.java
+++ app/src/main/java/com/example/welcome/kati6/SettingFragment.java
@@ -7,11 +7,62 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SettingFragment extends Fragment {
+
+    Button editacc, yourinfo, display,services,rate;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_setting,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        editacc=view.findViewById(R.id.setting1);
+        yourinfo=view.findViewById(R.id.setting2);
+        display=view.findViewById(R.id.setting3);
+        services=view.findViewById(R.id.setting4);
+        rate=view.findViewById(R.id.setting5);
+
+        editacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        yourinfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        display.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        services.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        rate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
